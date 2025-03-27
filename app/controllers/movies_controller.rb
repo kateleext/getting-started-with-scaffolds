2acd315292a433c17bc79d3709f54e0936b7aa95
kateleext <83829378+kateleext@users.noreply.github.com>
2025-03-27 01:49:56 +0000

    done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,7 @@ class MoviesController < ApplicationController
   end
 
   def show
-    the_id = params.fetch("path_id")
+    the_id = params.fetch("id")
 
     matching_movies = Movie.where({ :id => the_id })
 
@@ -17,23 +17,32 @@ class MoviesController < ApplicationController
     render({ :template => "movies/show" })
   end
 
-  def create
-    the_movie = Movie.new
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
-    the_movie.released = params.fetch("query_released")
+  def new
+    @the_movie = Movie.new
+  end
 
-    if the_movie.valid?
-      the_movie.save
-      redirect_to("/movies", { :notice => "Movie created successfully." })
+  def edit
+    @the_movie = Movie.where({:id => params.fetch("id")})[0]
+    render template: "movies/edit"
+  end
+
+  def create
+    @the_movie = Movie.new
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.released = params.fetch("query_released", false)
+
+    if @the_movie.valid?
+      @the_movie.save
+      redirect_to("/movies", { :notice => "Movie was successfully created" })
     else
-      redirect_to("/movies", { :alert => the_movie.errors.full_messages.to_sentence })
+      render template: "movies/new"
     end
   end
 
   def update
-    the_id = params.fetch("path_id")
-    the_movie = Movie.where({ :id => the_id }).at(0)
+    the_id = params.fetch("id")
+    the_movie = Movie.where({ :id => the_id })[0]
 
     the_movie.title = params.fetch("query_title")
     the_movie.description = params.fetch("query_description")
@@ -48,7 +57,7 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    the_id = params.fetch("path_id")
+    the_id = params.fetch("id")
     the_movie = Movie.where({ :id => the_id }).at(0)
 
     the_movie.destroy
